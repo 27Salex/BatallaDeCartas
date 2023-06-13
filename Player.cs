b595ef4d36c42408c6984975952eabb2033dde10
Alex <alaurencena.edt@gmail.com>
2023-06-13 20:01:08 +0200

    Player creado

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatallaDeCartas
+{
+    internal class Player
+    {
+        private int id;
+        private string name;
+        List<Carta> mano;
+        public List<Carta> Mano { get { return mano; } set { mano = value; } }
+        public int Id { get { return id; }}
+        public string Name { get { return name; }}
+    }
+}
